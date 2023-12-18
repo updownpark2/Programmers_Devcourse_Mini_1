3e1dacbfcd5bc546c01bcc766fc632a88964a44e
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-12-18 09:42:57 +0900

    feat: 회원가입 컴포넌트 개발

--- components/join.js
+++ components/join.js
@@ -1,1 +1,25 @@
-function Join() {}
+export function Join(root) {
+  root.innerHTML = `<div>
+<h1>회원가입</h1>
+<form>
+<input placeholder="id"/>
+<input placeholder="pwd" />
+<button>제출</button>
+</form>
+</div>`;
+
+  const form = document.querySelector("form");
+  form.onsubmit = onSubmit;
+
+  function inputReset(event) {
+    event.target[0].value = ``;
+    event.target[1].value = ``;
+  } // submit 후 input값을 reset
+
+  function onSubmit(event) {
+    event.preventDefault();
+    console.log(event.target);
+
+    inputReset(event);
+  }
+}
