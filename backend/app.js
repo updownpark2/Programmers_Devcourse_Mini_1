92268b23fb7306fe325da5ab97963b45c188dfeb
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-12-19 16:29:27 +0900

    feat: join컴포넌트에 api연동

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 // 여기서 server를 개발하자
 const express = require("express");
-const router = require("../routes/users");
+const userRouter = require("../backend/routes/users.js");
 const app = express();
-const userRouter = router;
+
 const cors = require("cors");
 
 app.use(express.json());
@@ -15,5 +15,5 @@ app.use("/users", userRouter);
 
 //여기서 각각 포트별로 정리를한다.
 
-app.listen(3000);
+app.listen(1234);
 //3000번으로 서버열었따.
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,11 +6,11 @@ router.use(express.json());
 const db = require("../db/users.js");
 
 router.post("/join", (req, res) => {
-  const { id, pwd } = req.body;
-
+  const { id, pwd, name } = req.body;
+  console.log(db);
   // 정상적으로 들어왔고 만약 id가 이미 있는거면 있다고 알려줘야함
   for (let i = 1; i <= db.size; i++) {
-    if (db.get(i) === id) {
+    if (db.get(i).id === id) {
       //걸리면?
       res.json({ message: "이미 존재하는 Id 입니다.", pass: false });
       return;
@@ -18,7 +18,7 @@ router.post("/join", (req, res) => {
   }
   //통과되면 없는거니까
   res.json({ message: "회원가입을 축하드립니다.", pass: true });
-  db.set(db.size + 1, { id: id, pwd: pwd });
+  db.set(db.size + 1, { id: id, pwd: pwd, name: name });
 });
 
 module.exports = router;
