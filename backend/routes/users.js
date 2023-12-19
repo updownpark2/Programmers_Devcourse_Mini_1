16b61b758c5383467f116ea0579f4fc866cd2bf6
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-12-19 16:41:59 +0900

    feat: login 컴포넌트에 API 적용

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -21,4 +21,19 @@ router.post("/join", (req, res) => {
   db.set(db.size + 1, { id: id, pwd: pwd, name: name });
 });
 
+router.post("/login", (req, res) => {
+  const { id, pwd } = req.body;
+  console.log(id, pwd);
+  // 정상적으로 들어왔고 만약 id가 이미 있는거면 있다고 알려줘야함
+  for (let i = 1; i <= db.size; i++) {
+    if (db.get(i).id === id && db.get(i).pwd === pwd) {
+      //걸리면?
+      res.json({ message: "반갑습니다.", pass: true });
+      return;
+    }
+  }
+  //통과되면 없는거니까
+  res.json({ message: "일치하지않습니다.", pass: false });
+});
+
 module.exports = router;
--- frontend/components/login.js
+++ frontend/components/login.js
@@ -1,10 +1,45 @@
+import { changeUrl } from "./index.js";
+
+window.addEventListener("click", (e) => {
+  if (e.target.id === "Login_Btn") {
+    let [id, pwd] = e.target.parentElement;
+    id = id.value;
+    pwd = pwd.value;
+    console.log(id, pwd);
+    PostIdPwd(id, pwd);
+    // 해당 id, pwd, name을 받아
+  }
+
+  e.preventDefault();
+});
 export function Login() {
   return `<div>
   <h1>로그인</h1>
   <form>
   <input placeholder="아이디"/>
   <input type="password" placeholder="패스워드"/>
-  <button>제출</button>
+  <button id="Login_Btn">제출</button>
   </form>
   </div>`;
+}
+
+async function PostIdPwd(id, pwd) {
+  const data = await fetch("http://localhost:1234/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: id,
+      pwd: pwd,
+    }),
+  });
+  const { message, pass } = await data.json();
+  if (pass) {
+    changeUrl("/mypage");
+    alert(message);
+  } else if (!pass) {
+    alert(message);
+    return;
+  }
 }
