4f674a95813376236749515996e82e7b2ee92db9
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-12-23 12:13:52 +0900

    DummyDB에서 mariaDB로 변경 및 회원기입, 로그인 수정

--- backend/db/channels.js
+++
@@ -1,4 +0,0 @@
-const db = new Map();
-db.set(1, { channelName: "괴물쥐", content: "게임" });
-
-module.exports = db;
--- backend/db/users.js
+++
@@ -1,4 +0,0 @@
-const db = new Map();
-db.set(1, { id: "d", pwd: "d", name: "d" });
-
-module.exports = db;
